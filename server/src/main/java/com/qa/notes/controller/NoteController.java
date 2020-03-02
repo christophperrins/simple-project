43f66379ffb613e45e9280238c785638a348056c
Chris Perrins <chrisperrins95@gmail.com>
2020-03-02 10:47:07 +0000

    api prepended to controller endpoints

--- server/src/main/java/com/qa/notes/controller/NoteController.java
+++ server/src/main/java/com/qa/notes/controller/NoteController.java
@@ -31,27 +31,27 @@ public class NoteController {
 	@Autowired
 	private NoteService service;
 
-	@RequestMapping(path = "note/", method = {RequestMethod.GET})
+	@RequestMapping(path = "/api/note", method = {RequestMethod.GET})
 	@ResponseStatus(code = HttpStatus.OK)
 	public List<NoteDto> getNotes(){
 		return new ArrayList<NoteDto>(service.getNotes());
 	}
 	
-	@RequestMapping(path = "note/", method = {RequestMethod.POST})
+	@RequestMapping(path = "/api/note", method = {RequestMethod.POST})
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public NoteDto createNote(@RequestBody NoteDto note){
 		return service.createNote(note);
 	}
 	
-	@RequestMapping(path = "note/", method = {RequestMethod.PUT})
+	@RequestMapping(path = "/api/note", method = {RequestMethod.PUT})
 	@ResponseStatus(code = HttpStatus.OK)
 	public NoteDto updateNote(@RequestBody NoteDto note) throws NotFoundException{
 		return service.updateNote(note);
 	}
 	
-	@RequestMapping(path = "note/{id}", method = {RequestMethod.DELETE})
+	@RequestMapping(path = "/api/note/{id}", method = {RequestMethod.DELETE})
 	@ResponseStatus(code = HttpStatus.NO_CONTENT)
-	public NoteDto deleteNote(@PathVariable Long id) throws NotFoundException{
+	public NoteDto deleteNote(@PathVariable("id") Long id) throws NotFoundException{
 		return service.deleteNote(id);
 	}
 }
